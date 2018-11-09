c42df0466d4a324522b6373826b8bec6086b6026
Logan Mace <lmmace92@hotmail.com>
2018-11-09 09:59:46 -0600

    recipes component added

---
+++ pages/recipes/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <section class="recipes">
+    <Recipe 
+      thumbnail="https://ichef.bbci.co.uk/food/ic/food_16x9_832/recipes/one_pot_chorizo_and_15611_16x9.jpg"
+      title="One Pot Spaghetti"
+      previewText="Delicious Pasta!"
+    />
+    <Recipe 
+      thumbnail="https://timedotcom.files.wordpress.com/2015/02/155278138-copy.jpg"
+      title="Sweet and Sour Pork"
+      previewText="Delicious Pork!"
+    />
+  </section>
+</template>
+
+<script>
+import Recipe from '@/components/Recipe';
+export default {
+  components: {
+    Recipe
+  }
+}
+</script>
+
+
+<style scoped>
+  .recipes {
+    display: flex;
+    flex-flow: row;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
